f1f94f556e0884266dbe66d8ffa7f296dfa3c36c
s-shiki <37012360+s-shiki@users.noreply.github.com>
2019-10-06 20:30:53 +0900

    changed dir location

---
+++ scripts/textlint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+textlint --format checkstyle --config .textlintrc article/*.re > \
+textlint.log || EXIT_CODE=$?
+
+cat textlint.log | bin/reviewdog -reporter=github-pr-review \
+	-diff='git --no-pager diff origin/master' \
+	-f=checkstyle \
+	-name=textlint
+
+exit ${EXIT_CODE}
